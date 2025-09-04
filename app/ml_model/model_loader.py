64033df8c37f8ae28e73d6097e859e8ad0474106
Gurveer Singh <150374263+gurveersingh25@users.noreply.github.com>
2025-09-04 22:27:21 +0530

    Update and rename download_model.py to model_loader.py

---
+++ app/ml_model/model_loader.py
@@ -0,0 +1,13 @@
+import os
+import gdown
+import joblib
+
+FILE_ID = "1rzNdMETYmnJGJWLgZOAErfY_GE6sxUZh"
+URL = f"https://drive.google.com/uc?id={FILE_ID}"
+MODEL_PATH = "app/ml_model/mudra_model.pkl"
+
+def get_model():
+    if not os.path.exists(MODEL_PATH):
+        print("Downloading the model from Google Drive...")
+        gdown.download(URL, MODEL_PATH, quiet=False)
+    return joblib.load(MODEL_PATH)
